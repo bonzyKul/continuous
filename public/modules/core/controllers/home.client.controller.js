4cffe0d41bc4c37ef89bb66c235705f3a808dd75
Alok Kulkarni <alokkuls@icloud.com>
2015-06-07 12:25:40 +0530

    updates

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -36,12 +36,26 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
                 {'title': 'item3', 'drag':true},
                 {'title': 'item4', 'drag':true},
                 {'title': 'item5', 'drag':true},
-                {'title': 'item6', 'drag':true}]},
-            {'name': 'Discovery',cards: []},
-            {'name': 'Design',cards: []},
-            {'name': 'Build',cards: []},
-            {'name': 'Pilot',cards: []}
+                {'title': 'item6', 'drag':true}], 'hideCol':false},
+            {'name': 'Discovery',cards: [], 'hideCol':false},
+            {'name': 'Design',cards: [], 'hideCol':false},
+            {'name': 'Build',cards: [], 'hideCol':false},
+            {'name': 'Pilot',cards: [], 'hideCol':false}
         ];
+
+
+        $scope.hiddenCol = function(column) {
+            angular.forEach($scope.columns, function(col) {
+                if(col.name === column.name) {
+                    if(column.hideCol == true) {
+                        column.hideCol = false;
+                    } else {
+                        column.hideCol = true;
+                    }
+                }
+            });
+        };
+
 
         $scope.addCard = function(column) {
             angular.forEach($scope.columns, function(col){
@@ -70,10 +84,30 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
             //containment: '#sortable-container2'
         };
 
+        $scope.removeCard = function(column, card) {
+            angular.forEach($scope.columns, function(col) {
+                if (col.name === column.name) {
+                    col.cards.splice(col.cards.indexOf(card), 1);
+                }
+            });
+        };
+
+
         $scope.dragControlListeners = {
             itemMoved: function (event) {
-                console.log(event.source.itemScope.modelValue.title);
-                $scope.release = prompt('Enter Release Info !');
+                //console.log(event.source.itemScope.modelValue.title);
+                //console.log(event.source.index);
+                //console.log(event.dest.sortableScope.$parent.column.name);
+                var columnName = event.dest.sortableScope.$parent.column.name;
+                if (columnName === 'Backlog') {
+                    $scope.release = "";
+                } else {
+                    $scope.release = prompt('Enter Release Info !');
+                    if ($scope.release === "") {
+                      $scope.release = "Rel";
+                    }
+                }
+
             }
         };
 	}
